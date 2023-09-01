131f6f9a02dc27702b29dc59381216c51198fd02
yungphilo <yungphilo@163.com>
2023-09-01 18:35:58 +0800

    update var.go

---
+++ var.go
@@ -0,0 +1,16 @@
+package main
+
+type PrometheusMetricValue struct {
+	Status string `json:"status"`
+	Data   struct {
+		ResultType string `json:"resultType"`
+		Result     struct {
+			Metric struct {
+				Name     string `json:"__name__,omitempty"`
+				Instance string `json:"instance,omitempty"`
+				Job      string `json:"job,omitempty"`
+			} `json:"metric"`
+			Value []string `json:"value"`
+		} `json:"result"`
+	} `json:"data"`
+}
