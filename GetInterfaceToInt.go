49c5faec90799dce0f8399a2b893b2ecbdd3ca01
yungphilo <yungphilo@163.com>
2023-09-12 17:07:33 +0800

    优化类型转化

--- GetInterfaceToInt.go
+++ GetInterfaceToInt.go
@@ -8,57 +8,43 @@ import (
 // 将interface转成int
 func GetInterfaceToInt(t1 interface{}) int {
 	var t2 int
-	switch t1.(type) {
+
+	switch t1 := t1.(type) {
 	case uint:
-		t2 = int(t1.(uint))
-		break
+		t2 = int(t1)
 	case int8:
-		t2 = int(t1.(int8))
-		break
+		t2 = int(t1)
 	case uint8:
-		t2 = int(t1.(uint8))
-		break
+		t2 = int(t1)
 	case int16:
-		t2 = int(t1.(int16))
-		break
+		t2 = int(t1)
 	case uint16:
-		t2 = int(t1.(uint16))
-		break
+		t2 = int(t1)
 	case int32:
-		t2 = int(t1.(int32))
-		break
+		t2 = int(t1)
 	case uint32:
-		t2 = int(t1.(uint32))
-		break
+		t2 = int(t1)
 	case int64:
-		t2 = int(t1.(int64))
-		break
+		t2 = int(t1)
 	case uint64:
-		t2 = int(t1.(uint64))
-		break
+		t2 = int(t1)
 	case float32:
-		t2 = int(t1.(float32))
-		break
+		t2 = int(t1)
 	case float64:
-		t2 = int(t1.(float64))
-		break
+		t2 = int(t1)
 	case string:
-		t2, _ = strconv.Atoi(t1.(string))
-		if t2 == 0 && len(t1.(string)) > 0 {
-			f, _ := strconv.ParseFloat(t1.(string), 64)
+		t2, _ = strconv.Atoi(t1)
+		if t2 == 0 && len(t1) > 0 {
+			f, _ := strconv.ParseFloat(t1, 64)
 			t2 = int(f)
 		}
-		break
 	case nil:
 		t2 = 0
-		break
 	case json.Number:
-		t3, _ := t1.(json.Number).Int64()
+		t3, _ := t1.Int64()
 		t2 = int(t3)
-		break
 	default:
 		t2 = t1.(int)
-		break
 	}
 	return t2
 }
@@ -66,57 +52,43 @@ func GetInterfaceToInt(t1 interface{}) int {
 // interface to float
 func GetInterfaceToFloat(t1 interface{}) float64 {
 	var t2 float64
-	switch t1.(type) {
+	switch t1 := t1.(type) {
 	case uint:
-		t2 = float64(t1.(uint))
-		break
+		t2 = float64(t1)
 	case int8:
-		t2 = float64(t1.(int8))
-		break
+		t2 = float64(t1)
 	case uint8:
-		t2 = float64(t1.(uint8))
-		break
+		t2 = float64(t1)
 	case int16:
-		t2 = float64(t1.(int16))
-		break
+		t2 = float64(t1)
 	case uint16:
-		t2 = float64(t1.(uint16))
-		break
+		t2 = float64(t1)
 	case int32:
-		t2 = float64(t1.(int32))
-		break
+		t2 = float64(t1)
 	case uint32:
-		t2 = float64(t1.(uint32))
-		break
+		t2 = float64(t1)
 	case int64:
-		t2 = float64(t1.(int64))
-		break
+		t2 = float64(t1)
 	case uint64:
-		t2 = float64(t1.(uint64))
-		break
+		t2 = float64(t1)
 	case float32:
-		t2 = float64(t1.(float32))
-		break
+		t2 = float64(t1)
 	// case float64:
 	// 	t2 = float64(t1.(float64))
 	// 	break
 	case string:
-		t2, _ = strconv.ParseFloat(t1.(string), 64)
-		if t2 == 0 && len(t1.(string)) > 0 {
-			f, _ := strconv.ParseFloat(t1.(string), 64)
+		t2, _ = strconv.ParseFloat(t1, 64)
+		if t2 == 0 && len(t1) > 0 {
+			f, _ := strconv.ParseFloat(t1, 64)
 			t2 = float64(f)
 		}
-		break
 	case nil:
 		t2 = 0
-		break
 	case json.Number:
-		t3, _ := t1.(json.Number).Int64()
+		t3, _ := t1.Int64()
 		t2 = float64(t3)
-		break
 	default:
 		t2 = t1.(float64)
-		break
 	}
 	return t2
 }
